95f1d3d53fcf6dc917264e2b1e261da55ae0f4e7
omrim <omriman11@gmail.com>
2021-04-08 19:15:45 +0300

    added a method give_container

--- src/algorithms/search/BestFirstSearch.java
+++ src/algorithms/search/BestFirstSearch.java
@@ -1,12 +1,18 @@
 package algorithms.search;
 
+import java.util.LinkedList;
 import java.util.PriorityQueue;
 import java.util.Queue;
 
 public class BestFirstSearch extends BreadthFirstSearch{
 
     public BestFirstSearch() {
-        unvisited = new PriorityQueue<>();
+        //unvisited = new PriorityQueue<>();
+    }
+
+    private Queue<AState>give_container(){
+        Queue<AState> unvisited = new PriorityQueue<>();
+        return unvisited;
     }
 
     @Override
