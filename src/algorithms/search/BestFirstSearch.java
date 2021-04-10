5de162d21d834e04795b873775e209c9037e41c6
elbam <elbamit@hotmail.com>
2021-04-10 14:51:34 +0300

    Small change - Deleted the give container method because we can initialize the correct container inside the constructor

--- src/algorithms/search/BestFirstSearch.java
+++ src/algorithms/search/BestFirstSearch.java
@@ -1,18 +1,13 @@
 package algorithms.search;
 
-import java.util.LinkedList;
 import java.util.PriorityQueue;
-import java.util.Queue;
 
 public class BestFirstSearch extends BreadthFirstSearch{
 
     public BestFirstSearch() {
-        //unvisited = new PriorityQueue<>();
+        unvisited = new PriorityQueue<>();
     }
 
-    public Queue<AState>give_container(){
-        return new PriorityQueue<>();
-    }
 
     @Override
     public String getName() {
